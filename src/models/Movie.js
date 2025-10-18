5001805ef1ac13be59b476797f5f017794be1c3a
Joss <joss.raine@gmail.com>
2025-10-18 19:37:55 +1100

    feat: created Movie model and schema, created seed script to fetch external API data and add to local database using environment variables

---
+++ src/models/Movie.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Movie title is required"],
+      trim: true,
+      maxlength: 200,
+    },
+    year: {
+      type: String,
+      required: true,
+      match: /^\d{4}$/,
+    },
+    director: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    genre: [
+      {
+        type: String,
+        required: true,
+        trim: true,
+      },
+    ],
+    plot: {
+      type: String,
+      maxlength: 1000,
+    },
+    actors: [
+      {
+        type: String,
+        required: true,
+        trim: true,
+      },
+    ],
+    imdbId: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^tt\d+$/, // Validates the id is tt followed by numbers
+    },
+    poster: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Movie", movieSchema);
