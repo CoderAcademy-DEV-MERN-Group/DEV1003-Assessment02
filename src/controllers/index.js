c04dc721039831676c8c42254af0c80c491d9107
Jordan Leal-Walker <16386@coderacademy.edu.au>
2025-10-21 16:25:28 +1100

    feat: created empty routes in all controllers and index.js to enable imports and route setting into server.js

---
+++ src/controllers/index.js
@@ -0,0 +1,15 @@
+import FriendshipController from './FriendshipController';
+import LeaderboardController from './LeaderboardController';
+import ListController from './ListController';
+import MovieController from './MovieController';
+import RatingController from './RatingController';
+import UserController from './UserController';
+
+export {
+  FriendshipController,
+  LeaderboardController,
+  ListController,
+  MovieController,
+  RatingController,
+  UserController,
+};
