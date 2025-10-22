b3d071b193c47b9cf5a31c71eb2eb9245e4d952d
Nhi Huynh <nhi.huynh@hotmail.com>
2025-10-23 05:05:19 +0800

    feat: add friendship model with basic friend system

--- src/models/Friendship.js
+++ src/models/Friendship.js
@@ -1,1 +1,45 @@
 // friendships will be the most basic version of friendships we can do successfully
+
+// 5. Friend System
+//    - Friendship model. Leverages:
+//      - User model
+//    - Friendship controller
+
+// friendship collection between two users, when user A sends a friend request and userB accepts
+
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const friendshipSchema = new Schema({
+  requesterUserId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  recipientUserId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  // confirmed: {
+  //   type: Boolean,
+  //   default: false,
+  // },
+  friendRequestStatus: {
+    type: String,
+    enum: ['pending', 'accepted', 'rejected'],
+    default: 'pending',
+  },
+});
+
+// prevent duplicate friend requests between same two users
+friendshipSchema.index(
+  {
+    requesterUserId: 1,
+    recipientUserId: 1,
+  },
+  { unique: true },
+);
+
+export default mongoose.model('Friendship', friendshipSchema);
