020aec9b1f0e06aa44481f306fc6ffbe47dff268
Jordan Leal-Walker <16386@coderacademy.edu.au>
2025-10-22 14:03:38 +1100

    fix: removed async database connection from server to change the module to a synchronous export, and to have connection occur in index while keeping pure logic in server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { app, HOST, PORT, databaseURL, connectToDatabase } from './server';
 
+// Only connect to database if not in test environment (tests use different db setup)
 if (process.env.NODE_ENV !== 'test') await connectToDatabase(databaseURL);
 
+// Start the server to listen for requests
 app.listen(PORT, HOST, () => {
   console.log(`Server is running at http://${HOST}:${PORT}`);
 });
