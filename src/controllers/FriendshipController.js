e66905654efcfdfa7dd0aba14446fb3710a99d51
Nhi Huynh <nhi.huynh@hotmail.com>
2025-10-23 05:09:49 +0800

    WIP: add scaffold for friendshipcontroller, need to test user model once valid users can be created first

--- src/controllers/FriendshipController.js
+++ src/controllers/FriendshipController.js
@@ -1,5 +1,28 @@
-import { Router } from 'express';
+import Friendship from '../models/Friendship';
+import User from '../models/User';
 
-const router = Router();
+// Controller for sending friend request
+// Route = POST /friends/request
 
-export default router;
+// export async function sendFriendRequest(request, respond) {
+//     try {
+//         const { requesterUserId, recipientUserId } = request.body;
+
+//         // Check if user exist
+
+//         // if user B does not exist, return invalid user error
+
+//         // If both users exist, create friendship request
+
+//         // Validate the existing status of friendship between both users
+
+// }
+
+// Controller to respond to a friend request to accept or reject
+// Route = POST /friends/respond
+
+// Controller to get friendship status
+// Route =GET /friends/status
+
+// Controller to get list of friends
+// Route = GET /friends/list/:userId
