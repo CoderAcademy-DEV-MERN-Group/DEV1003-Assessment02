c20fe704745432287dc12c83c001b7cdbbfc85f2
Nhi Huynh <nhi.huynh@hotmail.com>
2025-10-23 05:09:13 +0800

    WIP: add basic routes scaffold for friends. Need to do after testing users with friendship model then controllers next

--- src/routes/friends.js
+++ src/routes/friends.js
@@ -1,1 +1,29 @@
 // Routes to get friendship information (status etc)
+
+import { Router } from 'express';
+import {
+  sendFriendRequest,
+  respondToFriendRequest,
+  getFriendshipStatus,
+  getFriendsList,
+} from '../controllers/friendshipController.js';
+
+const router = Router();
+
+// Route to send a friend request
+// POST /friends/request
+router.post('/request', sendFriendRequest);
+
+// Route to respond to a friend request to accept or reject
+// POST /friends/respond
+router.post('/respond', respondToFriendRequest);
+
+// Route to get friendship status
+// GET /friends/status
+router.get('/status', getFriendshipStatus);
+
+// Route to get list of friends
+// GET /friends/list
+router.get('/list/:userId', getFriendsList);
+
+export default router;
