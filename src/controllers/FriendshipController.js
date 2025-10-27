90c087eac7fd1285b4c7f72b78bddb8e7d9366b2
Jordan Leal-Walker <16386@coderacademy.edu.au>
2025-10-27 16:01:16 +1100

    feat(FriendshipController): mapped out required logic in friendship controllers

--- src/controllers/FriendshipController.js
+++ src/controllers/FriendshipController.js
@@ -1,7 +1,66 @@
 import mongoose from 'mongoose';
 import Friendship from '../models/Friendship.js';
 import User from '../models/User.js';
-import { verifyToken } from '../utils/auth.js';
+// import { verifyToken } from '../utils/auth.js';
+// getAllFriendships,
+// getUserFriendships,
+// createFriendship,
+// updateFriendship,
+// removeFriendship,
+
+export const getAllFriendships = async (request, respond, next) => {
+  try {
+    // Database query to get all friendships documents
+    // Return the list of friendships
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserFriendships = async (request, respond, next) => {
+  try {
+    // Get id from request parameters or user object
+    // Database query to get friendships for the specific user
+    // Return the list of user's friendships
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createFriendship = async (request, respond, next) => {
+  try {
+    // Get user id from attached user object in request
+    // Get friend id from request body
+    // Create new friendship document in the database
+    // Return the created friendship
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateFriendship = async (request, respond, next) => {
+  try {
+    // Get friendship id from request parameters or attached user object
+    // Get update data from request body (maybe, if only handling changing bool then no need)
+    // Update the friendship document in the database
+    // Return the updated friendship
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const removeFriendship = async (request, respond, next) => {
+  try {
+    // Get friendship id from request parameters or attached user object
+    // Remove the friendship document from the database
+    // Return a success message with the removed friendship (maybe)
+  } catch (error) {
+    next(error);
+  }
+};
+
+// ----------------------------------------------------------------
+// OLD STUFF BELOW
 
 // get authenticated user's ID from request (auth middleware)
 // const getAuthenticatedUserId = (request) => {
