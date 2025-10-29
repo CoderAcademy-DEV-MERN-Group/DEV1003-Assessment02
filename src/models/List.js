a686063893bb15e276bf57e5deb1bbc7bb04c2a4
Nhi Huynh <nhi.huynh@hotmail.com>
2025-10-29 13:45:35 +0800

    refactor: added import of movie schema to movies field, removed pre-validation hook and refactored composite index for listSchema

--- src/models/List.js
+++ src/models/List.js
@@ -1,55 +1,26 @@
-// List is definitely a thing, we can give each list a description, created at and created by (eventually)
-
-// 5. List model requires:
-//    - movies: [mongoose.ObjectId, ref: 'Movie']
-//    - listName: String, required, unique
-//    - description: String
-//    - creator: mongoose.ObjectId, ref: 'User'
-//    - Discriminator: ReelCanon (fixed) CustomList (user-created)
-
-import mongoose from 'mongoose';
-import Movies from './Movie.js';
-
-const { Schema } = mongoose;
+import { Schema, model } from 'mongoose';
+import Movie from './Movie';
 
 // List schema for custom lists created by users
 const listSchema = new Schema(
   {
     // array of movie object IDs
-    movies: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Movie',
-        // the reel canon will have 100 movies from fixed seeded movies db,
-        // users can make their own customlists with selection of these movies
-        // required: true,  // should make required true? otherwise empty lists may be created
-      },
-    ],
+    movies: [Movie.schema],
     listName: {
       type: String,
       required: true,
-      unique: true,
       minlength: 2,
-      lowercase: true,
       trim: true,
     },
     description: {
       type: String,
-      // should we add min/max length or make default empty string if not required?
+      maxlength: 1000,
+      default: `Custom List for ${this.listName}`,
     },
     creator: {
       type: Schema.Types.ObjectId,
       ref: 'User',
       required: true,
-    },
-    // check to clarify discriminator section
-    // listType: {
-    //   type: String,
-    //   enum: {
-    //     values: ['ReelCanon', 'CustomList'],
-    isReelCanon: {
-      type: Boolean,
-      ref: Movies.Boolean,
     },
   },
   {
@@ -57,16 +28,7 @@ const listSchema = new Schema(
   },
 );
 
-// Ensure a user cannot create multiple lists with the same name
-// Check if customlist is unique per user or if it will be publicly unique
-listSchema.index({ creator: 1, listName: 1 }, { unique: true });
+// Composite index to ensure user cannot have duplicate listNames
+listSchema.index({ creator: 1, listName: 1 }, { unique: true, sparse: true }); // sparse allows multiple user to have 'null' listName
 
-// presave hook - prevent the reelcanon movie list from being changed or deleted
-listSchema.pre('save', function ValidateReelCannonModification(next) {
-  if (this.isReelCanon) {
-    if (this.isModified('movies')) {
-      const error = error('Reel Canon movie list cannot be modified or deleted');
-      return next(error);
-    }
-
-export default List = mongoose.model('List', listSchema);
+export default model('List', listSchema);
